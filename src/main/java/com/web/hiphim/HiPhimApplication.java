98717b0697619690ab7c912431a80306aa4b5506
Việt Anh Vũ <vietanhvu4298@gmail.com>
2019-02-14 19:58:40 +0700

    initialization

---
+++ src/main/java/com/web/hiphim/HiPhimApplication.java
@@ -0,0 +1,14 @@
+package com.web.hiphim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiPhimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiPhimApplication.class, args);
+    }
+
+}
+
