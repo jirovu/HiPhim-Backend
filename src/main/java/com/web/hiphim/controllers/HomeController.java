e7733bcf10453eef75b442a00797de2313558141
Việt Anh Vũ <vietanhvu4298@gmail.com>
2019-02-23 02:19:14 +0700

    Fix linh tinh

--- src/main/java/com/web/hiphim/controllers/HomeController.java
+++ src/main/java/com/web/hiphim/controllers/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
     public ResponseEntity<Optional<Movie>> user(@PathVariable String userId, @RequestParam("id") String id) {
         var userExist = userRepository.findById(userId);
         var movieExist = movieRepository.findById(id);
-        if (userExist != null && movieExist != null) {
+        if (!userExist.isEmpty() && !movieExist.isEmpty()) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(movieExist);
         }
